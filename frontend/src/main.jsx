b1425c196ad411afa40460a8000061d28dda8fa1
ROOKIEsarthak <officialsarthak22@gmail.com>
2024-02-19 16:31:53 +0530

    created and added pages to router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
