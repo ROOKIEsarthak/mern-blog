9f1c36f716a9dc0368adc893bba61c8de2733092
ROOKIEsarthak <officialsarthak22@gmail.com>
2024-02-21 19:49:24 +0530

    sign up page and functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
     const path = useLocation().pathname
   return (
     
-    <nav className='border-b-2 flex justify-between'>
+    <nav className='border-b-2 flex justify-between pl-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold :dark:text-white'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
-            via-purple-500 to-pink-500 rounded-lg text-white
+            via-purple-500 to-pink-500 rounded-lg  text-white
             '>Ambivert's</span>
             Blog
         </Link>
@@ -22,17 +22,19 @@ const Header = () => {
                     <SearchIcon />
                 </button>
         </form>
-        <div className='flex gap-2 md:order-2'>
-            <button className='w-12 h-10 hidden sm:inline' color='gray' >
+        <div className='flex gap-2 md:order-2 pt-1 pr-2'>
+            <button className='w-12 h-10 hidden sm:inline pb-2' color='gray' >
                 <FaMoon/>
             </button>
             <Link to={'/sign-in'} >
-                <button className='signin-button' style={{paddingBlock:"7px"}} >
+                <button className='px-2 py-1 bg-gradient-to-r from-indigo-500
+                via-purple-500 to-pink-500 rounded-lg text-white ' >
                     Sign In
                 </button>
+
             </Link>
         </div>
-        <nav className='border-b-2 flex justify-between'>
+        <nav className='border-b-2 flex justify-between pt-2'>
             <Link to={"/"} active={path==="/"} as={"div"}>
                 Home
             </Link>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import {ClipLoader} from "react-spinners"
+
 
 const SignUp = () => {
+
+  const [formData , setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading , setLoading ] = useState(false)
+
+  const showAlert1=()=>{
+    alert("Please fill out all the fields")
+  }
+
+  const showAlert2=(message)=>{
+    alert(message || "something went wrong")
+  }
+
+  
+  
+
+  const handleChange=(e)=>{
+    setFormData({...formData, [e.target.id]: e.target.value.trim()});
+    console.log(formData);
+
+  };
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if(!formData.username || !formData.email || !formData.password){
+      showAlert1()      
+      return;
+    }
+
+    
+    try {
+      setLoading(true)
+      const res = fetch('/api/user/sign-up',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json()
+      console.log(data);
+      console.log(data.message);
+
+      setLoading(false)
+      
+      // if(data.success === false){
+      //   // add alert here 
+      //   showAlert2(data.message)
+      //   return;
+      // }
+    } catch (error) {
+      
+      
+    }
+  }
+
+  
+
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <div className="flex-1">
+          {/*LEft */}
+
+          <Link to="/" className='font-bold :dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-500 rounded-lg text-white
+            '>Ambivert's</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project . You can sign up with your email and password 
+            or with Google .
+          </p>
+        </div>
+
+        <div className="flex-1">
+          {/*Right */}
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <label type="Your Username">
+                <input 
+                type="text"
+                placeholder='Username'
+                id='username' onChange={handleChange}/>
+              </label>
+            </div>
+            <div>
+              <label type="Your Email">
+                <input 
+                type="email"
+                placeholder='name@company.com'
+                id='email' onChange={handleChange}/>
+              </label>
+            </div>
+            <div>
+              <label type="Your Password">
+                <input 
+                type="password"
+                placeholder='Password'
+                id='password' onChange={handleChange}/>
+              </label>
+            </div>
+
+            <button className='px-2 py-1 bg-gradient-to-r from-indigo-500
+            via-purple-500 to-pink-500 rounded-lg text-white' type='submit' disabled={loading}>
+              {
+                loading ? ( 
+                  <>
+                    <ClipLoader color="#36d7b7"/>
+                    <span className='pl-3'>Loading....</span>
+                  </>
+                
+                 ) : "Sign Up"
+              }
+            </button >
+          </form>
+
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500' >
+              Sign In
+            </Link>
+          </div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
